406490e11f069b887bcc150193aec9271c3e990f
MBFDM <119826728+MBFDM@users.noreply.github.com>
2026-08-15 18:57:00 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- auth.py
+++ auth.py
@@ -3580,7 +3580,7 @@ def show_admin_dashboard():
                                 
                                 # Référence du document
                                 pdf.set_font('Arial', 'B', 10)
-                                pdf.cell(0, 0, f"DGF/EC-{avi_data['reference']}", 0, 1, 'C')
+                                pdf.cell(0, 0, f"DGF-EC / {avi_data['reference']}", 0, 1, 'C')
                                 pdf.ln(10)
                                 
                                 # ---- Logo et entête ----
@@ -3601,15 +3601,14 @@ def show_admin_dashboard():
                                 # ---- Corps du document ----
                                 pdf.set_font('Arial', '', 12)
                                 intro = [
-                                    "Nous soussignés, Eco Capital (E.C), établissement de microfinance agréé pour exercer des",
-                                    "activités bancaires en République du Congo conformément au décret n°7236/MEFB-CAB du",
-                                    "15 novembre 2007, après avis conforme de la COBAC D-2007/2018, déclarons avoir notre",
-                                    "siège au n°1636 Boulevard Denis Sassou Nguesso, Batignol Brazzaville.",
-                                    "",
-                                    "Représenté par son Directeur Général, Monsieur ILOKO Charmant.",
-                                    "",
-                                    f"Nous certifions par la présente que Monsieur/Madame {avi_data['nom_complet']}",
-                                    "détient un compte courant enregistré dans nos livres avec les caractéristiques suivantes :",
+                                    "Nous, soussignés, Eco Capital (E.C), Société à Responsabilité Limitée (SARL), constituée conformément au",
+                                    "droit OHADA ayant pour siège social sis au n°1636, Boulevard Denis Sassou Nguesso Batignolles,",
+                                    "Brazzaville , disposons d'un capital social de 60 000 000 Xaf, soit 91 469,94 euros. Immatriculée au Registre",
+                                    "du Commerce et du Crédit Mobilier sous le numéro RCCM/BZV/B12/00320-NIUM24000000665934H, et",
+                                    "agréée par les autorités monétaires sous le numéro n°078/MFBPP/AETF/DR-SAR-BOTC, conformément aux",
+                                    "disposition légales en vigueur du règlement COBAC EMF R-2017/01.",
+                                    f"Nous certifions par la présente que {avi_data['nom_complet']} détient un compte",
+                                    "courant enregistré dans nos livres avec les caractéristiques suivantes :",
                                     ""
                                 ]
                                 
@@ -3619,33 +3618,32 @@ def show_admin_dashboard():
                                 # Informations bancaires en gras
                                 pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(40, 5, "CODE BANQUE :", 0, 0)
-                                pdf.set_font('Arial', '', 12)
+                                pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(0, 5, avi_data['code_banque'], 0, 1)
                                 
                                 pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(45, 5, "NUMERO COMPTE : ", 0, 0)
-                                pdf.set_font('Arial', '', 12)
+                                pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(0, 5, avi_data['numero_compte'], 0, 1)
                                 
                                 pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(20, 5, "Devise :", 0, 0)
-                                pdf.set_font('Arial', '', 12)
+                                pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(0, 5, avi_data['devise'], 0, 1)
                                 pdf.ln(5)
                                 
                                 # ---- Détails du virement ----
                                 details = [
-                                    f"Il est l'ordonnateur d'un virement irrévocable et permanent d'un montant total de {avi_data['montant']:,.2f} FCFA",
-                                    f"({montant_en_lettres(avi_data['montant'])}), équivalant actuellement à {avi_data['montant']/650:,.2f} euros,",
-                                    "destiné à couvrir les frais liés à ses études en France.",
+                                    f"Il est l'ordonnateur d'un virement irrévocable et permanent d'un montant total de {avi_data['montant']} FCFA",
+                                    f"({montant_en_lettres(avi_data['montant'])}), équivalant actuellement à {avi_data['montant']/650} euros, cette somme est destinée à couvrir les frais liés",
+                                    "à ses études en France.",
                                     "",
-                                    "Il est précisé que ce compte demeurera bloqué jusqu'à la présentation, par le donneur",
-                                    "d'ordre, de ses nouvelles coordonnées bancaires ouvertes en France.",
+                                    "Il est précisé que ce compte demeurera bloqué jusqu'à la présentation, par le donneur d'ordre, de ses",
+                                    "nouvelles coordonnées bancaires ouvertes en France.",
                                     "",
                                     "À défaut, les fonds ne pourront être remis à sa disposition qu'après présentation de son",
                                     "passeport attestant d'un refus de visa. Toutefois, nous autorisons le donneur d'ordre, à",
                                     "toutes fins utiles, à utiliser notre compte ouvert auprès de United Bank for Africa (UBA).",
-                                    ""
                                 ]
                                 
                                 for line in details:
@@ -3654,12 +3652,12 @@ def show_admin_dashboard():
                                 # ---- Coordonnées bancaires ----
                                 pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(16, 5, "IBAN :", 0, 0)
-                                pdf.set_font('Arial', '', 12)
+                                pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(0, 5, avi_data['iban'], 0, 1)
                                 
                                 pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(16, 5, "BIC :", 0, 0)
-                                pdf.set_font('Arial', '', 12)
+                                pdf.set_font('Arial', 'B', 12)
                                 pdf.cell(0, 5, avi_data['bic'], 0, 1)
                                 pdf.ln(10)
                                 
@@ -3687,9 +3685,19 @@ def show_admin_dashboard():
                                     "Brazzaville République du Congo"
                                 ]
                                 
-                                pdf.set_font('Arial', 'I', 10)
+                                pdf.set_font('Arial', '', 10)
                                 for line in footer:
                                     pdf.cell(1, 4.5, line, 0, 2, 'L')
+
+                                # ---- Pied de page ----
+                                fin = [
+                                    "The purpose of this AVI is to confirm the existence of an account in our records. The undersigned assumes no obligation or commitment of any kind. This document connot be considered as a guarantee",
+                                    "endorsement, surety, or any other similar form of commitment. The signatory diclaims all liability for any damage resulting from the improper, exaggerated, or abusive use of this AVI.",
+                                ]
+                                
+                                pdf.set_font('Arial', '', 3)
+                                for line in fin:
+                                    pdf.cell(1, 4.5, line, 0, 2, 'C')
                                 
                                 # ---- QR Code ----
                                 qr_data = {
